1412de1aa20678da083e9836ea460df81e3eb03c
OwlNerd <aeb55s@missouristate.edu>
2022-12-10 20:28:45 -0600

    Tic Tac Toe Horizontal

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -5,26 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.hardware.display.DisplayManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button[] [] buttons;
+    //private Button[] [] buttons;
     private Context context;
     private TicTacToe tttGame;
     private TextView status;
+    private Button b1, b2, b3, b4, b5, b6, b7, b8, b9;
+
+    public static String MA = "MainActivity";
+    public static int ACTION_BAR_HEIGHT = 56; // vertical, in dp units
+
+    private float pixelDensity;
+    private boolean verticalDimensionsSet;
+    public static int screenHeightInVP;
+    private int spacingInVP;
+
+    private boolean horizontalDimensionsSet;
+    public static int screenHeightInHP;
+    private int spacingInHP;
+
+    //private Button b1, b2, b3;
+    private int actionBarHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +53,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tttGame = new TicTacToe();
         buildGUIByCode();
+
     }
 
     private void buildGUIByCode() {
-
         //Get width of the screen
         Point size = new Point();
         getWindowManager().getDefaultDisplay().getSize(size);
@@ -47,22 +68,67 @@ public class MainActivity extends AppCompatActivity {
         gridLayout.setRowCount(TicTacToe.SIDE + 1);
 
         // Create the buttons and add them to the GridLayout
-
-        buttons = new Button[TicTacToe.SIDE][TicTacToe.SIDE];
+        //buttons = new Button[TicTacToe.SIDE][TicTacToe.SIDE];
+        b1 = new Button(this);
+        b2 = new Button(this);
+        b3 = new Button(this);
+        b4 = new Button(this);
+        b5 = new Button(this);
+        b6 = new Button(this);
+        b7 = new Button(this);
+        b8 = new Button(this);
+        b9 = new Button(this);
         ButtonHandler bh = new ButtonHandler();
 
         for (int row = 0; row <TicTacToe.SIDE; row ++) {
             for (int col = 0; col < TicTacToe.SIDE; col++) {
-                buttons[row][col] = new Button(this);
-                buttons[row][col].setTextSize((int)(w * .2));
-                buttons[row][col].setOnClickListener(bh);
+                //buttons[row][col] = new Button(this);
+                b1 = new Button(this);
+                b2 = new Button(this);
+                b3 = new Button(this);
+                b4 = new Button(this);
+                b5 = new Button(this);
+                b6 = new Button(this);
+                b7 = new Button(this);
+                b8 = new Button(this);
+                b9 = new Button(this);
+                //buttons[row][col].setTextSize((int)(w * .2));
+                b1.setTextSize((int)(w * .2));
+                b2.setTextSize((int)(w * .2));
+                b3.setTextSize((int)(w * .2));
+                b4.setTextSize((int)(w * .2));
+                b5.setTextSize((int)(w * .2));
+                b6.setTextSize((int)(w * .2));
+                b7.setTextSize((int)(w * .2));
+                b8.setTextSize((int)(w * .2));
+                b9.setTextSize((int)(w * .2));
+                //buttons[row][col].setOnClickListener(bh);
+                b1.setOnClickListener(bh);
+                b2.setOnClickListener(bh);
+                b3.setOnClickListener(bh);
+                b4.setOnClickListener(bh);
+                b5.setOnClickListener(bh);
+                b6.setOnClickListener(bh);
+                b7.setOnClickListener(bh);
+                b8.setOnClickListener(bh);
+                b9.setOnClickListener(bh);
 
-                gridLayout.addView(buttons[row][col], w, w);
+
+                //gridLayout.addView(buttons[row][col], w, w);
+                gridLayout.addView(b1, w, w);
+                gridLayout.addView(b2, w, w);
+                gridLayout.addView(b3, w, w);
+                gridLayout.addView(b4, w, w);
+                gridLayout.addView(b5, w, w);
+                gridLayout.addView(b6, w, w);
+                gridLayout.addView(b7, w, w);
+                gridLayout.addView(b8, w, w);
+                gridLayout.addView(b9, w, w);
+
             }
         }
 
         // Set up layout paramaters for the 4th row of the gridlayout
-
         status = new TextView(this);
         GridLayout.Spec rowSpec = GridLayout.spec(TicTacToe.SIDE, 1);
         GridLayout.Spec columnSpec = GridLayout.spec(0, TicTacToe.SIDE);
@@ -84,13 +150,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(gridLayout);
     }
 
+
     private class ButtonHandler implements View.OnClickListener{
         @Override
         public void onClick(View v) {
             Log.w("MainActivity", "Inside onClick, v=" + v);
             for (int row = 0; row < TicTacToe.SIDE; row ++)
                 for(int column = 0; column < TicTacToe.SIDE; column ++)
-                    if(v == buttons[row][column])
+                    /*if(v == buttons[row][column])
+                        update(row,column);*/
+                    if(v == b1)
                         update(row,column);
 
         }
@@ -100,9 +169,11 @@ public class MainActivity extends AppCompatActivity {
         Log.w("MainActivity", "Inside update: " + row + ", " + column);
         int play = tttGame.play(row, column);
         if (play == 1)
-            buttons[row][column].setText("X");
+            //buttons[row][column].setText("X");
+            b1.setText("X");
         else if (play == 2)
-            buttons[row][column].setText("O");
+            //buttons[row][column].setText("O");
+            b1.setText("O");
         if (tttGame.isGameOver()) {// Game over, disable buttons
             status.setBackgroundColor(Color.RED);
             enableButtons(false);
@@ -112,7 +183,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showNewGameDialog() {
-
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setTitle("This is fun");
         alert.setMessage("Play again?");
@@ -125,13 +195,16 @@ public class MainActivity extends AppCompatActivity {
     private void resetButtons() {
         for(int row = 0; row <TicTacToe.SIDE; row++)
             for (int col = 0; col <TicTacToe.SIDE; col++)
-                buttons[row][col].setText("");
+                //buttons[row][col].setText("");
+                b1.setText("");
+
     }
 
     private void enableButtons(boolean enabled) {
         for(int row = 0; row <TicTacToe.SIDE; row++)
             for (int col = 0; col <TicTacToe.SIDE; col++)
-                buttons[row][col].setEnabled(enabled);
+                //buttons[row][col].setEnabled(enabled);
+                b1.setEnabled(enabled);
 
 
     }
